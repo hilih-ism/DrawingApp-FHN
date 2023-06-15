7229608337e56b201658f73b3a93c908a70f1890
Hildana Shewaneka <112654424+hilih-ism@users.noreply.github.com>
2023-06-16 01:02:10 +0300

    last update

--- Adv Prog Assign/src/main/java/module-info.java
+++ Adv Prog Assign/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.advprogassig {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.java;
 
 
     opens com.example.advprogassig to javafx.fxml;
